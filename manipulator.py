9a1486a0f03855e8a624cb189a4f41d205777df5
gdao-research <gdao.research@gmail.com>
2019-04-06 02:32:19 -0400

    add gripper & orientation

--- manipulator.py
+++ manipulator.py
@@ -1,137 +1,184 @@
 import numpy as np
 import cv2
 
-class Manipulator2R(object):
-    def __init__(self, links=(18, 13), total_steps=50, max_rot_angle=np.pi/18):
-        assert sum(links) < 38  # Constraint length
-        self.img_size = 84  # Constraint image size to draw
 
-        self.links = np.asarray(links, dtype='float32')
-        self._total_steps = total_steps
-        self._rot_angle_range = np.asarray([-max_rot_angle, max_rot_angle], dtype='float32')
-        self.nb_actions = len(links)
-        self.desired_goal = np.zeros(len(links), dtype='float32')
+class Manipulator(object):
+    def __init__(self, base, current_angles, links=(18, 13)):
+        self._links = np.array(links, dtype='float32')
+        self._base = base
+        self._current_angles = np.array(current_angles, dtype='float32')
 
-    def _wrap_to_pi(self, angle):
-        angle = (angle/np.pi*180 + 180) % 360
-        if angle < 0:
-            angle += 360
-        angle -= 180
-        angle = angle/180*np.pi
-        return angle
+    def wrap_to_PI(self, angles):
+        ret = np.remainder(angles, 2*np.pi)
+        mask = np.abs(ret) > np.pi
+        ret[mask] -= 2*np.pi*np.sign(ret[mask])
+        return ret
 
-    def reset(self):
-        self.available_steps = self._total_steps
-        self.current_angles = np.random.uniform(-np.pi, np.pi, size=self.links.shape).astype('float32')
-        self.end_points = self._compute_end_effectors()
-        assert len(self.end_points) == len(self.links) + 1
-        self.current_pos = self.end_points[-1]
+    def set_current_angles(self, current_angles):
+        self._current_angles = np.array(current_angles)
 
-        self.desired_goal = self._sample_new_goal()
-        while not self._is_reachable_goal(self.desired_goal) or self.is_goal(self.current_pos, self.desired_goal):  # Make sure new goal is reachable & initially not starting configuration
-            self.desired_goal = self._sample_new_goal()
-        # self.desired_goal = self.current_pos
-        self.goal_draw = self._draw_goal()
-        return self._update_canvas()
+    def get_current_angles(self):
+        return self._current_angles
 
-    def step(self, action):
-        assert len(action) == self.nb_actions
-        self.available_steps -= 1
-        self.current_angles += action
-        for i in range(len(self.current_angles)):
-            if self.current_angles[i] > np.pi or self.current_angles[i] < -np.pi:
-                self.current_angles[i] = self._wrap_to_pi(self.current_angles[i])
-        self.end_points = self._compute_end_effectors()
-        self.current_pos = self.end_points[-1]
-        obs = self._update_canvas()
-        reached = self.is_goal(self.current_pos, self.desired_goal)
-        if reached:
-            return obs, 0.0, True, {}
-        else:
-            if self.available_steps:
-                return obs, -1.0, False, {}
-            else:
-                return obs, -1.0, True, {}
-
-    def is_goal(self, current, desired):
-        dist = np.sqrt(np.sum((current - desired)**2))
-        return True if dist < 2 else False
-
-    def _compute_end_effectors(self):
+    def compute_end_points(self, rot_angles):
+        assert len(rot_angles) == len(self._links) + 1
+        self._current_angles = self.wrap_to_PI(
+            self._current_angles + np.array(rot_angles))
         x = 0
         y = 0
-        end_points = [np.copy([self.img_size//2, self.img_size//2])]
-        for i in range(len(self.links)):
-            x += int(self.links[i]*np.cos(np.sum(self.current_angles[:i+1])))
-            y += int(self.links[i]*np.sin(np.sum(self.current_angles[:i+1])))
-            end_points.append(np.copy([x + self.img_size//2, y + self.img_size//2]))
+        end_points = [np.array([self._base[0], self._base[1]])]
+        for i in range(len(self._links)):
+            x += int(self._links[i]*np.cos(np.sum(self._current_angles[:i+1])))
+            y += int(self._links[i]*np.sin(np.sum(self._current_angles[:i+1])))
+            end_points.append(np.array([x + self._base[0], y + self._base[1]]))
         return end_points
 
-    def _sample_new_goal(self):
-        return np.random.randint(0, self.img_size, size=self.nb_actions)
 
-    def _is_reachable_goal(self, goal):
-        goal_to_O_dist = np.sqrt((goal[0] - self.img_size//2)**2 + (goal[1] - self.img_size//2)**2)
-        return True if (self.links[0] - self.links[1]) < goal_to_O_dist < (self.links[0] + self.links[1]) else False
+class ManipulatorEnvironment(object):
+    def __init__(self, max_movements=50, links=(160, 120), img_size=640):
+        assert sum(links) < img_size/2 - 10
+        self._size = img_size
+        self.link_colors = [[0], [1]]
+        self._base = np.array([int(round(self._size/2)), int(round(self._size/2))])
+        self._goal = None
+        self.goal_img = None
+        self._links = links
+        self.max_movements = max_movements
+        self.available_moves = max_movements
+        self.nb_actions = len(links) + 1
+        self.manipulator = Manipulator(self._base, np.zeros(self.nb_actions), self._links)
+        self.end_points = self.manipulator.compute_end_points(np.zeros(self.nb_actions))
+
+    def set_goal(self, goal=None):
+        while not self._goal_is_reachable(goal):
+            goal = self._sample_new_goal()
+        self._goal = np.array(goal)
+
+    def get_goal(self):
+        return self._goal
+
+    def compute_reward(self, end_effector, angle, goal=None):
+        if goal is None:
+            goal = np.copy(self._goal)
+        if self.is_goal(goal, end_effector, angle):
+            return 0
+        return -1
+
+    def is_goal(self, goal, pos, angle):
+        # Goal is defined as almost same location & orientation different is less than 10 degree
+        # -1 index is orientation
+        return np.linalg.norm(goal[:-1] - pos) < 10 and np.abs(goal[-1] - angle) < np.pi/18
+
+    def _goal_is_reachable(self, goal):
+        if goal is None:
+            return False
+        dist = np.linalg.norm(np.array(goal[:2]) - self._base)
+        return (self._links[0] - sum(self._links[1:])) < dist < sum(self._links)
+
+    def _sample_new_goal(self):
+        xy = np.random.randint(0, self._size, size=2)
+        orientation = np.random.uniform(-np.pi, np.pi)
+        return np.asarray([xy[0], xy[1], orientation])
+
+    def draw_goal(self, goal=None):
+        if goal is None:
+            goal = np.copy(self._goal)
+        ret = np.ones((self._size, self._size), dtype='uint8')
+        ret[int(goal[0]), int(goal[1])] = 255
+        k = np.asarray([[1, 1, 1], [1, 1, 1], [0, 1, 0]], dtype='uint8')
+        ret = cv2.dilate(ret, k, iterations=20)
+        M = cv2.getRotationMatrix2D((goal[1], goal[0]), goal[2]/np.pi*180, 1)
+        ret = cv2.warpAffine(ret, M, ret.shape)
+        return ret.reshape((self._size, self._size, 1))
 
     def _connect_end_points(self, end0, end1):
         d = end1 - end0
         j = np.argmax(np.abs(d))
         D = d[j]
         aD = np.abs(D)
-        points = end0 + (np.outer(np.arange(aD+1), d) + (aD>>1))//aD
+        points = end0 + (np.outer(np.arange(aD+1), d) + (aD >> 1))//aD
         return (points[:, 0], points[:, 1])
 
-    def _draw_link(self, end0, end1, colors, color_scale):
-        points = self._connect_end_points(end0, end1)
-        temp = np.zeros((self.img_size, self.img_size), dtype='uint8')
-        temp[points] = 255
-        strel = np.asarray([[0, 1, 0], [1, 1, 1], [0, 1, 0]], dtype='uint8')
-        temp = cv2.dilate(temp, strel, iterations=1)
-        final = np.zeros((self.img_size, self.img_size, 3), dtype='uint8')
-        for i, c in enumerate(colors):
-            final[:, :, c] = (temp*color_scale[i]).astype('uint8')
-        return final
+    def _draw_links(self):
+        ret = np.zeros((self._size, self._size, 3), dtype='uint8')
+        for i in range(len(self.end_points)-1):
+            points = self._connect_end_points(
+                self.end_points[i], self.end_points[i+1])
+            temp = np.zeros((self._size, self._size), dtype='uint8')
+            temp[points] = 255
+            k = np.asarray([[1, 1, 1], [1, 1, 1], [1, 1, 1]], dtype='uint8')
+            temp = cv2.dilate(temp, k, iterations=4)
+            for c in self.link_colors[i]:
+                ret[:, :, c] += temp
+        return np.clip(ret, 0, 255)
 
-    def _draw_end_effector(self):
-        strel = np.asarray([[1, 1, 1], [1, 1, 1], [1, 1, 1]], dtype='uint8')
-        temp = np.zeros((self.img_size, self.img_size), dtype='uint8')
-        temp[self.current_pos[0], self.current_pos[1]] = 255
-        temp = cv2.dilate(temp, strel, iterations=4)
-        final = np.zeros((self.img_size, self.img_size, 3), dtype='uint8')
-        final[:, :, 1] = temp  # end effector is green
-        return final
+    def _draw_end_effector(self, angle):
+        end_effector = self.end_points[-1]
+        temp = np.zeros((self._size, self._size), dtype='uint8')
+        temp[end_effector[0], end_effector[1]] = 255
+        k = np.asarray([[1, 1, 1], [1, 1, 1], [0, 1, 0]], dtype='uint8')
+        temp = cv2.dilate(temp, k, iterations=22)
+        M = cv2.getRotationMatrix2D(
+            (end_effector[1], end_effector[0]), angle/np.pi*180, 1)
+        temp = cv2.warpAffine(temp, M, temp.shape)
+        ret = np.zeros((self._size, self._size, 3), dtype='uint8')
+        ret[:, :, 2] = temp
+        return ret
 
-    def _draw_goal(self):
-        strel = np.asarray([[0, 1, 0], [1, 1, 1], [0, 1, 0]], dtype='uint8')
-        temp = np.zeros((self.img_size, self.img_size), dtype='uint8')
-        temp[self.desired_goal[0], self.desired_goal[1]] = 255
-        temp = cv2.dilate(temp, strel, iterations=3)
-        final = np.zeros((self.img_size, self.img_size, 3), dtype='uint8')
-        final[:, :, 0] = temp  # goal is red
-        return final
+    def _draw(self, angle):
+        links_img = self._draw_links()
+        end_effector_img = self._draw_end_effector(angle)
+        return np.clip(links_img + end_effector_img, 0, 255)
 
-    def _update_canvas(self):
-        colors = [[2], [1, 2]]
-        colors_scale = [[1], [0.3, 1]]
-        canvas = [self.goal_draw]
-        for i in range(len(self.end_points) - 1):
-            canvas.append(self._draw_link(self.end_points[i], self.end_points[i+1], colors[i], colors_scale[i]))
-        canvas.append(self._draw_end_effector())
-        canvas = np.clip(np.sum(canvas, axis=0), 0, 255)
-        return canvas
+    def reset(self, current_angles=None):
+        if current_angles is None:
+            current_angles = np.random.uniform(-np.pi, np.pi, size=self.nb_actions)
+        self.manipulator.set_current_angles(current_angles)
+        self.available_moves = self.max_movements
+        self.end_points = self.manipulator.compute_end_points(current_angles)
+        while self._goal is None or self.is_goal(self._goal, self.end_points[-1], current_angles[-1]):
+            self.set_goal()
+        # self.set_goal(self.end_points[-1])  # Test goal is initial configuratuion
+        self.goal_img = self.draw_goal()
+        obs = self._draw(current_angles[-1])
+        info = {'end_points': self.end_points,
+                'angles': self.manipulator.get_current_angles(), 'links': self._links}
+        return np.concatenate([obs, self.goal_img], axis=2), info
+
+    def step(self, action):
+        self.available_moves -= 1
+        self.end_points = self.manipulator.compute_end_points(action)
+        obs = self._draw(self.manipulator.get_current_angles()[-1])
+        reward = self.compute_reward(self.end_points[-1], action[-1])
+        done = True if not self.available_moves or reward == 0 else False
+        info = {'end_points': self.end_points,
+                'angles': self.manipulator.get_current_angles(), 'links': self._links}
+        return np.concatenate([obs, self.goal_img], axis=2), reward, done, info
 
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
-    env = Manipulator2R()
-    s = env.reset()
-    ns, r, d, _ = env.step(np.asarray([np.pi/2, -np.pi/2]))
-    fig = plt.figure()
-    fig.add_subplot(121)
-    plt.imshow(s)
-    plt.title('s')
-    fig.add_subplot(122)
-    plt.imshow(ns)
-    plt.title('ns')
-    plt.show()
+
+    def plot(ss):
+        fig = plt.figure()
+        nb = len(ss)
+        for i, s in enumerate(ss):
+            fig.add_subplot(nb*100 + 20 + i*nb+1)
+            plt.imshow(s[:, :, :3])
+            plt.title('state')
+            plt.axis('off')
+            fig.add_subplot(nb*100 + 20 + i*nb+2)
+            plt.imshow(s[:, :, 3], cmap='gray')
+            plt.title('goal')
+            plt.axis('off')
+        plt.show()
+
+    env = ManipulatorEnvironment()
+    s, info = env.reset()
+    print(s.shape, info)
+    # s = cv2.resize(s, (84, 84))
+    # plot(s)
+
+    ns, r, d, info = env.step(np.asarray([0, np.pi/2, np.pi/3]))
+    print(ns.shape, r, d, info)
+    plot([s, ns])
